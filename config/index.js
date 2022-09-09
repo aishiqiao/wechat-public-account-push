aedde69c0167b6a2477f07c871bfa2b59f826815
aishiqiao <113193566+aishiqiao@users.noreply.github.com>
2022-09-09 23:04:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_SECRET: "ccf40952a24292e7bfc94e616664180c",
+   APP_SECRET: "3fd1a0c828229fcf30bc4203f762e251",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
