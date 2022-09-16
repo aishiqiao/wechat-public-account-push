ed83b419ab4978bf96702f49ea5ceb7101c018a1
aishiqiao <113193566+aishiqiao@users.noreply.github.com>
2022-09-16 20:31:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_ID: "wxcc6cd328336ec00a",
+   APP_ID: "wx128d8fdd8e63b47e",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_SECRET: "3fd1a0c828229fcf30bc4203f762e251",
+   APP_SECRET: "2339e15872af457a4df19619123e2056",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -35,21 +35,21 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "of8tj6F45sadGxjVUyuqWJRU2tjc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dqPDxpkgveN8MRC6y8RnfDcCaztCdi89PGF-De_jeSg",
+      useTemplateId: "RLQlwtADppB3rAfjvN_LIQ_T7ZBNaRxMRfDPXjr8NFU",
       // 所在省份
       province: "河南",
       // 所在城市
       city: "商丘",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.cnblogs.com/pijunqi/"
+      openUrl: "https://m.tianqi.com/xiayi/15/"
     }, 
     {
       name: "老公", 
       id: "of8tj6F45sadGxjVUyuqWJRU2tjc",
-      useTemplateId: "dqPDxpkgveN8MRC6y8RnfDcCaztCdi89PGF-De_jeSg",
+      useTemplateId: "RLQlwtADppB3rAfjvN_LIQ_T7ZBNaRxMRfDPXjr8NFU",
       province: "河南",
       city: "商丘",
-      openUrl: "https://www.cnblogs.com/pijunqi/"
+      openUrl: "https://m.tianqi.com/xiayi/15/"
     }, 
   ],
 
